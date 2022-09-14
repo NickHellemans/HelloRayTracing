e7e3b39d6748ce4cd0d7e8c81ff809dbccceaffc
NickHellemans <60689153+NickHellemans@users.noreply.github.com>
2022-09-14 02:31:43 +0200

    Refactor into renderer class

---
+++ RayTracing/src/Renderer.cpp
@@ -0,0 +1,45 @@
+﻿#include "Renderer.h"
+
+#include "Walnut/Random.h"
+
+void Renderer::OnResize(uint32_t width, uint32_t height)
+{
+
+    if(m_FinalImage)
+    {
+        //Exit function when no resize is needed so delete doesnt get called
+        if(m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height)
+            return;
+        //Personal function inside Walnut::Image | Will check if image needs resizing, if so -> release and reallocate memory
+        //Better design than to delete image and create new one + the object pointer doesnt change, just the contents of the memory block
+        //So everything that holds a reference to this object still has same image because we dont delete object and create new one that will
+        //different spot in memory
+        m_FinalImage->Resize(width, height);
+    }
+    else
+    {
+        //Create image if it doesnt exist only no need to recreate it when width/height dont match current image size
+        //this gets handles in resize function
+        m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
+    }
+
+    //Delete current image data if exists - its ok to call delete on nullptr(first render) - delete will check for u
+    delete[] m_ImageData;
+    //Reallocate for new data
+    //uint32 is 32 bits -> RGBA 1 byte for each color = 32bits
+    m_ImageData = new uint32_t[width * height];
+}
+
+void Renderer::Render()
+{
+    //Render every pixel of viewport
+    //Fill image data
+    for (uint32_t i = 0; i < m_FinalImage->GetWidth() * m_FinalImage->GetHeight(); i++) {
+        m_ImageData[i] = Walnut::Random::UInt(); // 0xffff00ff : From l->r: ABGR, 1 byte each == 2 letters
+        m_ImageData[i] |= 0xff000000; //Or equals to set bits of alpha channel to 1 (most significant) because
+        //random uint will give us random alpha values aswell and we want to see image
+    }
+
+    //Upload to gpu for rendering - Inefficient to load data on cpu and transfer to GPU but will change later in series
+    m_FinalImage->SetData(m_ImageData);
+}
--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -1,3 +1,4 @@
+#include "Renderer.h"
 #include "Walnut/Application.h"
 #include "Walnut/EntryPoint.h"
 #include "Walnut/Image.h"
@@ -27,8 +28,9 @@ public:
 		m_ViewportHeight= ImGui::GetContentRegionAvail().y;
 
 		//Display image if it exists
-		if(m_Image)
-			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+		auto image = m_Renderer.GetFinalImage();
+		if(image)
+			ImGui::Image(image->GetDescriptorSet(), { (float)image->GetWidth(), (float)image->GetHeight() });
 		ImGui::End();
 		//Pop style var once window ends
 		ImGui::PopStyleVar();
@@ -38,34 +40,14 @@ public:
 
 	void Render() {
 		Timer timer;
-		//Create image if it doesnt exist or recreate it when width/height dont match current image size
-		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportWidth != m_Image->GetHeight())
-		{
-			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
-			//Delete current image data if exists
-			delete[] m_ImageData; 
-			//Reallocate for new data
-			//uint32 is 32 bits -> RGBA 1 byte for each color = 32bits
-			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
-		}
-
-		//fill image data
-		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++) {
-			m_ImageData[i] = Random::UInt(); // 0xffff00ff : From l->r: ABGR, 1 byte each == 2 letters
-			m_ImageData[i] |= 0xff000000; //Or equals to set bits of alpha channel to 1 (most significant) because random uint will give us random alpha values aswell and we want to see image
-
-		}
-
-		//Upload to gpu for rendering - Inefficient to load data on cpu and transfer to GPU but will change later in series
-		m_Image->SetData(m_ImageData);
-
+		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
+		m_Renderer.Render();
 		//Set timer to see how long it took to render
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 
 private:
-	std::shared_ptr<Image> m_Image;
-	uint32_t* m_ImageData = nullptr; 
+	Renderer m_Renderer;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 	float m_LastRenderTime = 0.0f;
 };
